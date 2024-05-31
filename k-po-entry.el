6180444d93a8c582cf0eef58a4da792d181b1419
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-31 17:25:47 +0900

    Port entry getters to an entry datatype

---
+++ k-po-entry.el
@@ -0,0 +1,82 @@
+;;; k-po-entry.el --- Entry object -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;; Functions for the entry object type.
+
+;;; Code:
+
+(require 'cl-lib)
+(require 'k-po-extract)
+
+(cl-defstruct (k-po-entry (:copier nil)
+                          (:constructor k-po-entry))
+  "An object representing an entry in a PO file."
+  start msgctxt-start msgid-start msgid_plural-start msgstr-block-start
+  msgstr-form-start msgstr-form-end
+  end
+  (type nil :documentation "The type of an entry.
+
+Possible values: `obsolete', `fuzzy', `untranslated', or `translated'"))
+
+(defun k-po--entry-from-vars ()
+  "Return a `k-po-entry' from the variables set by `k-po-find-span-of-entry'."
+  (k-po-entry :start k-po-start-of-entry
+              :msgctxt-start k-po-start-of-msgctxt
+              :msgid-start k-po-start-of-msgid
+              :msgid_plural-start k-po-start-of-msgid_plural
+              :msgstr-block-start k-po-start-of-msgstr-block
+              :msgstr-form-start k-po-start-of-msgstr-form
+              :msgstr-form-end k-po-end-of-msgstr-form
+              :end k-po-end-of-entry
+              :type k-po-entry-type))
+
+(defun k-po-entry-msgid (entry)
+  "Return the msgid text of ENTRY."
+  (k-po-extract-unquoted (current-buffer)
+                         (k-po-entry-msgid-start entry)
+                         (or (k-po-entry-msgid_plural-start entry)
+                             (k-po-entry-msgstr-block-start entry))))
+
+(defun k-po-entry-msgid_plural (entry)
+  "Return the unquoted msgid_plural string from ENTRY.
+Return nil if it is not present."
+  (when (k-po-entry-msgid_plural-start entry)
+    (k-po-extract-unquoted (current-buffer)
+                           (k-po-entry-msgid_plural-start entry)
+                           (k-po-entry-msgstr-block-start entry))))
+
+(defun k-po-entry-msgstr-flavor (entry)
+  "Helper function to detect msgstr and msgstr[] variants of ENTRY.
+Returns one of \"msgstr\" or \"msgstr[i]\" for some i."
+  (save-excursion
+    (goto-char (k-po-entry-msgstr-form-start entry))
+    (re-search-forward "^\\(#~[ \t]*\\)?\\(msgstr\\(\\[[0-9]\\]\\)?\\)")
+    (match-string 2)))
+
+(defun k-po-entry-msgstr-form (entry)
+  "Extract and return the unquoted msgstr string for ENTRY."
+  (k-po-extract-unquoted (current-buffer)
+                         (k-po-entry-msgstr-form-start entry)
+                         (k-po-entry-msgstr-form-end entry)))
+
+(defun k-po-entry-comment (entry)
+  "Return the editable comment of ENTRY in the uncommented form."
+  (let ((buffer (current-buffer))
+        (obsolete (eq (k-po-entry-type entry) 'obsolete)))
+    (save-excursion
+      (goto-char (k-po-entry-start entry))
+      (if (re-search-forward k-po-comment-regexp (k-po-entry-end entry) t)
+          (with-temp-buffer
+            (insert-buffer-substring buffer (match-beginning 0) (match-end 0))
+            (goto-char (point-min))
+            (while (not (eobp))
+              (if (looking-at (if obsolete "#\\(\n\\| \\)" "# ?"))
+                  (replace-match "" t t))
+              (forward-line 1))
+            (buffer-string))
+        ""))))
+
+(provide 'k-po-entry)
+
+;;; k-po-entry.el ends here
--- k-po-mode.el
+++ k-po-mode.el
@@ -34,6 +34,9 @@
 
 (require 'f)
 
+(require 'k-po-extract)
+(require 'k-po-entry)
+
 
 ;;; Emacs portability matters - part I.
 ;;; Here is the minimum for customization to work.  See part II.
@@ -1188,6 +1191,11 @@ fuzzy, untranslated, or translated."
     ;; Put the cursor back where it was.
     (goto-char here)))
 
+(defun k-po-current-entry ()
+  "Return the current entry at point, as an entry object."
+  (k-po-find-span-of-entry)
+  (k-po--entry-from-vars))
+
 (defun k-po-add-attribute (name)
   "Add attribute NAME to the current entry, unless it is already there."
   (save-excursion
@@ -1516,91 +1524,6 @@ no entries of the other types."
   (k-po-display-current-entry))
 
 ;;; Killing and yanking fields.
-
-(defun k-po-extract-unquoted (buffer start end)
-  "Extract and return the unquoted string in BUFFER going from START to END.
-Crumb preceding or following the quoted string is ignored."
-  (save-excursion
-    (goto-char start)
-    (search-forward "\"")
-    (setq start (point))
-    (goto-char end)
-    (search-backward "\"")
-    (setq end (point)))
-  (k-po-extract-part-unquoted buffer start end))
-
-(defun k-po-extract-part-unquoted (buffer start end)
-  "Extract and return the unquoted string in BUFFER going from START to END.
-Surrounding quotes are already excluded by the position of START and END."
-  (with-temp-buffer
-   (insert-buffer-substring buffer start end)
-   ;; Glue concatenated strings.
-   (goto-char (point-min))
-   (while (re-search-forward "\"[ \t]*\\\\?\n\\(#~\\)?[ \t]*\"" nil t)
-     (replace-match "" t t))
-   ;; Remove escaped newlines.
-   (goto-char (point-min))
-   (while (re-search-forward "\\\\[ \t]*\n" nil t)
-     (replace-match "" t t))
-   ;; Unquote individual characters.
-   (goto-char (point-min))
-   (while (re-search-forward "\\\\[\"abfnt\\0-7]" nil t)
-     (cond ((eq (preceding-char) ?\") (replace-match "\"" t t))
-           ((eq (preceding-char) ?a) (replace-match "\a" t t))
-           ((eq (preceding-char) ?b) (replace-match "\b" t t))
-           ((eq (preceding-char) ?f) (replace-match "\f" t t))
-           ((eq (preceding-char) ?n) (replace-match "\n" t t))
-           ((eq (preceding-char) ?t) (replace-match "\t" t t))
-           ((eq (preceding-char) ?\\) (replace-match "\\" t t))
-           (t (let ((value (- (preceding-char) ?0)))
-                (replace-match "" t t)
-                (while (looking-at "[0-7]")
-                  (setq value (+ (* 8 value) (- (following-char) ?0)))
-                  (replace-match "" t t))
-                (insert value)))))
-   (buffer-string)))
-
-(defun k-po-call-requoted (func prefix obsolete)
-  "Call FUNC, which inserts a string, and return the string fully requoted.
-If PREFIX, precede the result with its contents.
-If OBSOLETE, comment all generated lines in the returned string.
-FUNC should insert the wanted string in the buffer which is
-current at the time of evaluation. If FUNC is itself a string,
-then this string inserted."
-  (with-temp-buffer
-    (if (stringp func)
-        (insert func)
-      (push-mark)
-      (funcall func))
-    (goto-char (point-min))
-    (let ((multi-line (re-search-forward "[^\n]\n+[^\n]" nil t)))
-      (goto-char (point-min))
-      (while (re-search-forward "[\"\a\b\f\n\r\t\\]" nil t)
-        (cond ((eq (preceding-char) ?\") (replace-match "\\\"" t t))
-              ((eq (preceding-char) ?\a) (replace-match "\\a" t t))
-              ((eq (preceding-char) ?\b) (replace-match "\\b" t t))
-              ((eq (preceding-char) ?\f) (replace-match "\\f" t t))
-              ((eq (preceding-char) ?\n)
-               (replace-match (if (or (not multi-line) (eobp))
-                                  "\\n"
-                                "\\n\"\n\"")
-                              t t))
-              ((eq (preceding-char) ?\r) (replace-match "\\r" t t))
-              ((eq (preceding-char) ?\t) (replace-match "\\t" t t))
-              ((eq (preceding-char) ?\\) (replace-match "\\\\" t t))))
-      (goto-char (point-min))
-      (if prefix (insert prefix " "))
-      (insert (if multi-line "\"\"\n\"" "\""))
-      (goto-char (point-max))
-      (insert "\"")
-      (if prefix (insert "\n"))
-      (if obsolete
-          (progn
-            (goto-char (point-min))
-            (while (not (eobp))
-              (or (eq (following-char) ?\n) (insert "#~ "))
-              (search-forward "\n"))))
-      (buffer-string))))
 
 (defun k-po-get-msgid ()
   "Extract and return the unquoted msgid string."
