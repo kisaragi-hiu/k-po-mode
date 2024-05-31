7327365f8731dbda56b387ec864e0307355dae46
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-05-31 16:34:01 +0900

    Rename; prepare for transplant of commits

--- Cask
+++ Cask
@@ -1,7 +1,7 @@
 (source melpa)
 (source gnu)
 
-(package-file "pkgname.el")
+(package-file "k-po-mode.el")
 
 (development
  (depends-on "buttercup"))
--- tests/test-k-po-mode.el
+++ tests/test-k-po-mode.el
@@ -1,10 +1,10 @@
 ;; -*- mode: lisp-interaction; lexical-binding: t; -*-
 
-(require 'pkgname)
+(require 'k-po-mode)
 (require 'buttercup)
 
 (describe "hello"
   (it "says hello"
-    (expect (pkgname-hello-world)
+    (expect (k-po-mode-hello-world)
             :to-equal
             "Hello world!")))
