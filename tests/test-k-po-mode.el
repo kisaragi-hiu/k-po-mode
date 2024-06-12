4def6c92e609a5a23bc26a14c5794de13067c545
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-06-12 23:20:50 +0900

    start adding fixture test

--- tests/test-k-po-mode.el
+++ tests/test-k-po-mode.el
@@ -6,6 +6,16 @@
 (require 'k-po-mode)
 (require 'buttercup)
 
+(defmacro test--with-fixture (file &rest body)
+  "Run BODY within a buffer that has FILE opened."
+  (declare (indent 1))
+  (setq file (concat "tests/" file))
+  `(progn
+     (unless (file-exists-p ,file)
+       (error "%s does not exist" ,(file-truename file)))
+     (with-current-buffer (find-file-noselect ,file)
+       ,@body)))
+
 (describe "language code"
   (it "works"
     (expect (k-po--language->code "English")
@@ -14,3 +24,10 @@
     (expect (k-po--language<-code "ja")
             :to-equal
             "Japanese")))
+
+(describe "navigation"
+  (it "gets the right language"
+    (test--with-fixture "fixtures/a.po"
+      (expect (k-po-current-target-language)
+              :to-equal
+              "zh_TW"))))
